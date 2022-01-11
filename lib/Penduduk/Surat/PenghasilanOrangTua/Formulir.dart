1ee5d3b0ce171659cb4af5a7719365399cbe7849
awenwijaya <61014772+awenwijaya@users.noreply.github.com>
2022-01-12 01:27:39 +0800

    Tampilan Form SP Penghasilan Orang Tua

--- lib/Penduduk/Surat/PenghasilanOrangTua/Formulir.dart
+++ lib/Penduduk/Surat/PenghasilanOrangTua/Formulir.dart
@@ -27,6 +27,164 @@ class _formSPPenghasilanOrangTuaState extends State<formSPPenghasilanOrangTua> {
             onPressed: (){Navigator.of(context).pop();},
           ),
         ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'images/paycheck.png',
+                  height: 100,
+                  width: 100,
+                ),
+                margin: EdgeInsets.only(top: 20),
+              ),
+              Container(
+                child: Text(
+                  "Pengajuan SP Penghasilan Orang Tua",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: HexColor("#025393")
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                margin: EdgeInsets.only(top: 10),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "1. Data Orang Tua",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                margin: EdgeInsets.only(top: 30, left: 20),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Silahkan pilih data orang tua yang akan dibuat pernyataan gajinya. Bisa data Ibu ataupun data Ayah yang Anda masukkan.\n\nData orang tua ini nantinya akan otomatis dimasukkan ke dalam berkas surat ketika berkas surat sudah di verifikasi.",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 14
+                  ),
+                ),
+                padding: EdgeInsets.only(left: 30, right: 30),
+                margin: EdgeInsets.only(top: 10),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: (){},
+                  child: Text(
+                    "Pilih Data Orang Tua",
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                      color: HexColor("#025393"),
+                      fontWeight: FontWeight.w700
+                    ),
+                  ),
+                  color: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(color: HexColor("#025393"), width: 2)
+                  ),
+                  padding: EdgeInsets.only(top: 10, bottom: 10, left: 50, right: 50),
+                ),
+                margin: EdgeInsets.only(top: 20),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 28, vertical: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        borderSide: BorderSide(color: HexColor("#025393"))
+                      ),
+                      hintText: "Gaji Orang Tua (Dalam Rupiah)",
+                      prefixIcon: Icon(Icons.attach_money)
+                    ),
+                    keyboardType: TextInputType.number,
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14
+                    ),
+                  ),
+                ),
+                margin: EdgeInsets.only(top: 20),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "2. Keperluan",
+                  style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700
+                  ),
+                ),
+                margin: EdgeInsets.only(top: 30, left: 20),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Silahkan masukkan keperluan Anda dalam mengurus surat ini.\n\nKeperluan Anda nantinya akan otomatis dimasukkan ke dalam berkas surat ketika berkas surat sudah di verifikasi",
+                  style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14
+                  ),
+                ),
+                padding: EdgeInsets.only(left: 30, right: 30),
+                margin: EdgeInsets.only(top: 10),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 28, vertical: 8),
+                  child: TextField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: HexColor("#025393"))
+                      ),
+                      hintText: "Keperluan Anda"
+                    ),
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14
+                    ),
+                  ),
+                ),
+                margin: EdgeInsets.only(top: 20),
+              ),
+              Container(
+                child: FlatButton(
+                  child: Text(
+                    "Ajukan SP Penghasilan Orang Tua",
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                      color: HexColor("#025393"),
+                      fontWeight: FontWeight.w700
+                    ),
+                  ),
+                  color: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(color: HexColor("#025393"), width: 2)
+                  ),
+                  padding: EdgeInsets.only(top: 10, bottom: 10, left: 50, right: 50),
+                ),
+                margin: EdgeInsets.only(top: 20, bottom: 20),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
